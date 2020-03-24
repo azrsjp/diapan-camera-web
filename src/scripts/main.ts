fbca533cef1d550cc753f36c1486591f6d30ea62
azrsjp <tt@azrs.jp>
2020-03-24 23:32:03 +0900

    Using camera test

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,1 +1,25 @@
-console.log('diapan');
+import { Constants } from './constants';
+
+const video = <HTMLVideoElement>document.getElementById('video');
+
+window.onload = () => {
+  navigator.mediaDevices
+    .getUserMedia(Constants.kMedias)
+    .then((stream) => {
+      return new Promise((resolve, reject) => {
+        video.srcObject = stream;
+        video.onloadedmetadata = () => {
+          resolve();
+        };
+        video.onerror = (error) => {
+          reject(error);
+        };
+      });
+    })
+    .then(() => {
+      console.log('camera initialized');
+    })
+    .catch((err) => {
+      console.log('cant use camera', err);
+    });
+};
