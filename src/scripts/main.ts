10a31f63626664cca0a16759acb9cc47acc73d05
azrsjp <tt@azrs.jp>
2020-03-24 23:48:57 +0900

    Adjust camera size

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,6 +1,22 @@
 import { Constants } from './constants';
+import { Utility } from './utility';
 
 const video = <HTMLVideoElement>document.getElementById('video');
+
+const adjustViews = () => {
+  const [windowWidth, windowHeight] = Utility.getWindowSize();
+  const [videoWidth, videoHeight] = Utility.aspectFit(
+    Constants.kExpectedPhotoWidth,
+    Constants.kExpectedPhotoHeight,
+    windowWidth,
+    windowHeight
+  );
+
+  video.style.width = videoWidth + 'px';
+  video.style.height = videoHeight + 'px';
+  video.style.top = Math.floor((windowHeight - videoHeight) * 0.5) + 'px';
+  video.style.left = Math.floor((windowWidth - videoWidth) * 0.5) + 'px';
+};
 
 window.onload = () => {
   navigator.mediaDevices
@@ -18,6 +34,7 @@ window.onload = () => {
     })
     .then(() => {
       console.log('camera initialized');
+      adjustViews();
     })
     .catch((err) => {
       console.log('cant use camera', err);
