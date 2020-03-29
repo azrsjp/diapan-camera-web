01e6f6163710ccde91c03f71596b6d481c0aa9d6
gomachan_7 <tt@a-azarashi.jp>
2020-03-29 18:23:12 +0900

    Add Capturer

---
+++ src/scripts/capturer.ts
@@ -0,0 +1,41 @@
+export class Capturer {
+  private canvas: HTMLCanvasElement = null;
+  private images: { img: HTMLImageElement; url: string }[] = [];
+
+  constructor() {
+    this.canvas = document.createElement('canvas');
+  }
+
+  getCanvas = () => {
+    return this.canvas;
+  };
+
+  saveAsImage = () => {
+    return new Promise((resolve, reject) => {
+      this.canvas.toBlob((blob) => {
+        this.clearCanvas();
+
+        var img = document.createElement('img');
+        var url = URL.createObjectURL(blob);
+        this.images.push({ img, url });
+
+        img.onload = () => {
+          resolve(img);
+        };
+        img.src = url;
+      });
+    });
+  };
+
+  clear = () => {
+    this.images.forEach((element) => {
+      URL.revokeObjectURL(element.url);
+    });
+    this.images = [];
+  };
+
+  private clearCanvas() {
+    const ctx = this.canvas.getContext('2d');
+    ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+  }
+}
