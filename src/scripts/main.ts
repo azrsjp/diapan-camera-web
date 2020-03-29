de94da05dcef2803371afb6c11bb42fe3a8b7e18
azrsjp <tt@azrs.jp>
2020-03-29 18:23:12 +0900

    Add Capturer

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,8 +1,10 @@
 import { Constants } from './constants';
 import { Utility } from './utility';
 import { VideoLayer } from './video-layer';
+import { Capturer } from './capturer';
 
 const video = <HTMLVideoElement>document.getElementById('video');
+const capturer = new Capturer();
 const videoLayer = new VideoLayer(video);
 
 const adjustViews = () => {
@@ -22,6 +24,20 @@ const adjustViews = () => {
   video.style.height = videoHeight + 'px';
   video.style.top = Math.floor((windowHeight - videoHeight) * 0.5) + 'px';
   video.style.left = Math.floor((windowWidth - videoWidth) * 0.5) + 'px';
+
+  const [canvasContentWidth, canvasContentHeight] = Utility.aspectFit(
+    videoWidth,
+    videoHeight,
+    trueVideoWidth,
+    trueVideoHegiht
+  );
+  const captureCanvas = capturer.getCanvas();
+  captureCanvas.setAttribute('width', canvasContentWidth.toString());
+  captureCanvas.setAttribute('height', canvasContentHeight.toString());
+  captureCanvas.style.width = videoWidth + 'px';
+  captureCanvas.style.height = videoHeight + 'px';
+  captureCanvas.style.top = Math.floor((windowHeight - videoHeight) * 0.5) + 'px';
+  captureCanvas.style.left = Math.floor((windowWidth - videoWidth) * 0.5) + 'px';
 };
 
 window.onload = () => {
